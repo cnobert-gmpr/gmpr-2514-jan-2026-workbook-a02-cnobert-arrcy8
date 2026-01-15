bd1646fd923c8f6bca4817e4ae10c8c4b3755a5f
4rrcy <rsaberola1@nait.ca>
2026-01-15 11:38:48 -0700

    lesson 3

--- Lesson03/LoopGame.cs
+++ Lesson03/LoopGame.cs
@@ -2,18 +2,22 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
-namespace Lesson02;
+namespace Lesson03;
 
-public class IntroGame : Game
+public class LoopGame : Game
 {
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
-    private Texture2D _pixel;
-    private float _xPosition = 180, _yPosition = 150;
-    private float _speed = 150;
-    private int _width = 80, _height = 50;
 
-    public IntroGame ()
+    private Texture2D _pixel;
+    private Vector2 _position, _dimensions;
+
+    private int _count;
+    private float _spacing;
+
+    private Rectangle[] _rectangles;
+
+    public LoopGame()
     {
         _graphics = new GraphicsDeviceManager(this);
         Content.RootDirectory = "Content";
@@ -22,6 +26,21 @@ public class IntroGame : Game
 
     protected override void Initialize()
     {
+        _position = new Vector2(50, 200);
+        _dimensions = new Vector2(60,40);
+        _count = 6;
+        _spacing = 10;
+
+        _rectangles = new Rectangle[_count];
+
+        for(int c = 0; c < _count; c++)
+        {
+            float x = _position.X + c * (_dimensions.X + _spacing);
+
+            _rectangles[c] = new Rectangle((int)x, (int)_position.Y,
+             (int)_dimensions.X, (int)_dimensions.Y);
+
+        }
 
         base.Initialize();
     }
@@ -31,17 +50,11 @@ public class IntroGame : Game
         _spriteBatch = new SpriteBatch(GraphicsDevice);
         _pixel = new Texture2D(GraphicsDevice, 1, 1);
         _pixel.SetData(new [] {Color.White});
-
     }
 
     protected override void Update(GameTime gameTime)
     {
-        base.Update(gameTime);
-
-        _xPosition += _speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
-
-        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-            Exit();
+        //_position.X += 60 * (float)gameTime.ElapsedGameTime.TotalSeconds;
 
         base.Update(gameTime);
     }
@@ -51,9 +64,12 @@ public class IntroGame : Game
         GraphicsDevice.Clear(Color.CornflowerBlue);
 
         _spriteBatch.Begin();
-        Rectangle rect = new Rectangle(100, 150, 80, 50);
-        _spriteBatch.Draw(_pixel, rect, Color.White);
-        
+
+        foreach(Rectangle r in _rectangles)
+        {
+           _spriteBatch.Draw(_pixel, r, Color.Aquamarine); 
+        }
+
         _spriteBatch.End();
 
         base.Draw(gameTime);
